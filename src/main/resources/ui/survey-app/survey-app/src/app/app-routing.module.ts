1373b0c3dd799b4e3e69a5f825ba127797692d94
Yahya ShqairS <y.shqair@hotmail.com>
2021-02-22 20:39:15 +0200

    set the basic design for login page.

--- src/main/resources/ui/survey-app/survey-app/src/app/app-routing.module.ts
+++ src/main/resources/ui/survey-app/survey-app/src/app/app-routing.module.ts
@@ -13,6 +13,10 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'survey',
+    loadChildren: () => import('./survey/survey.module').then( m => m.SurveyPageModule)
+  },
 
 ];
 
