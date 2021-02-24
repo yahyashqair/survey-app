beb61f61666ca4225ccb60ff58e1451f39f38dcc
Yahya ShqairS <y.shqair@hotmail.com>
2021-02-24 21:34:48 +0200

    implement questions UI and Handler.

--- src/main/resources/ui/survey-app/survey-app/src/app/components/question/question.component.ts
+++ src/main/resources/ui/survey-app/survey-app/src/app/components/question/question.component.ts
@@ -8,15 +8,23 @@ import {Question} from '../../model/question';
 })
 export class QuestionComponent implements OnInit {
 
-  questions: Question[] = [{type: 'trueAndFalse', body: 'Are you Yahya?'},
-    {type: 'range', body: 'Are you Yahya?'},
-    {type: 'range', body: 'Are you Yahya?'},
-    {type: 'range', body: 'Are you Yahya?'}];
+  questions: Question[] = [{type: 'trueAndFalse', body: 'True or False?'},
+    {type: 'text', body: 'Short answer?'},
+    {type: 'testArea', body: 'Long answer?'},
+    {type: 'multiChoice', body: 'Select One?'},
+    {type: 'range', body: 'Select from Range?'}];
+
+  answers = {};
 
   constructor() {
   }
 
   ngOnInit() {
   }
+
+  handleText(question, event) {
+    console.log(event.target.value);
+  }
+
 
 }
